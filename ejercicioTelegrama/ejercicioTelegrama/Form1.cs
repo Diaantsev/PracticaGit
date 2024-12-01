2d207f58793c6cc8d1ebd5a1c6154ddc55c5f2e8
DAS2425 <dithedy@gmail.com>
2024-12-01 12:57:26 +0100

    Cambios finales de precio

--- ejercicioTelegrama/ejercicioTelegrama/Form1.cs
+++ ejercicioTelegrama/ejercicioTelegrama/Form1.cs
@@ -35,7 +35,7 @@ namespace ejercicioTelegrama
             {
                 if (numPalabras <= 10)
                 {
-                    coste = 2.5;
+                    coste = 3;
                 }
                 else
                 {
@@ -49,7 +49,7 @@ namespace ejercicioTelegrama
                 {
                     if (numPalabras <= 10)
                     {
-                        coste = 5;
+                        coste = 6;
                     }
                     else
                     {
